9cec932f307a83bcaac879ef18188d5c5cdb6666
Richard Avalos <130488668+Richard-avalos@users.noreply.github.com>
2025-10-11 14:28:13 -0600

    Initial commit

---
+++ src/main/java/com/ravalos/hex_spring_template/HexSpringTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.ravalos.hex_spring_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexSpringTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexSpringTemplateApplication.class, args);
+	}
+
+}
