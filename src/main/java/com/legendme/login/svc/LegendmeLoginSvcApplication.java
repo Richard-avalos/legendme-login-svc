0d0d1913a6eb51ec65450613fcfbe8fece33b992
Richard Avalos <avalosrichard@gmail.com>
2025-11-03 16:50:22 -0600

    Agregando mejora en el servicio

--- src/main/java/com/legendme/login/svc/LegendmeLoginSvcApplication.java
+++ src/main/java/com/legendme/login/svc/LegendmeLoginSvcApplication.java
@@ -7,9 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LegendmeLoginSvcApplication {
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("CLIENT_ID", dotenv.get("CLIENT_ID"));
-		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+//		Dotenv dotenv = Dotenv.load();
+//		System.setProperty("CLIENT_ID", dotenv.get("CLIENT_ID"));
+//		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
 		SpringApplication.run(LegendmeLoginSvcApplication.class, args);
 	}
 
